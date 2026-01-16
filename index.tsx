bf11467bc244d7cce4493117b5b20bf60d3a8744
junnosukeg8181cc <63033773+junnosukeg8181cc@users.noreply.github.com>
2026-01-17 01:09:35 +0900

    修正

--- index.tsx
+++ index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // これが重要！
+import './index.css'; // これを絶対に追加や！
 
 const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error("Could not find root element");
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
